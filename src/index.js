5cae33b367a3c14155b3c184a121fd0d766cc93e
Filip Tadic <fike300705@gmail.com>
2025-04-10 01:01:43 +0200

    Finished Logic for Page-Switching

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import './styles.css';
 import { homePage } from './homePage.js';
 import { menuPage } from './menuPage.js';
 
-homePage();
+const pageLoader = {
+  home: document.querySelector('#homeButton'),
+  menu: document.querySelector('#menuButton'),
+  about: document.querySelector('#aboutButton'),
+  
+  loadHome: function() {
+    const content = document.querySelector('#content');
+    content.replaceChildren();
+    homePage();
+  },
+  loadMenu: function() {
+    const content = document.querySelector('#content');
+    content.replaceChildren();
+    menuPage();
+  },
+  init: function() {
+    this.home.addEventListener('click', () => { this.loadHome()});
+    this.menu.addEventListener('click', () => { this.loadMenu()});
+  }
+}
+
+pageLoader.init();
